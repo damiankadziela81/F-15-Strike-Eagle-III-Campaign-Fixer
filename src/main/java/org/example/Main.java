d08d715b69afa43afb87755d7de51bf3464d77b0
damiankadziela81 <damiankadziela02@gmail.com>
2023-03-24 20:44:47 +0100

    Removed the while loop since you only use either option anyway and quit. Added checks to see if there was no file present - arrays are of length 0, before handing them to reconstructTargets method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,42 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        while(true){
-            System.out.println("\nF-15 Strike Eagle 3 Campaign Fixer");
-            System.out.println("Choose your option:");
-            System.out.println("[1] Make backup (before mission)");
-            System.out.println("[2] Fix campaign file (after mission)");
-            System.out.println("[Q] Quit");
-            Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.nextLine().toLowerCase();
-            switch(userInput) {
-                case "1" -> {
-                    String fileName = FileNameBuilder.buildFileName();
-                    FileHandler.makeBackup(fileName,"backup" + fileName);
-                }
-                case "2" -> {
-                    String fileName = FileNameBuilder.buildFileName();
-                    Byte[] arrayFixed = DataHandler.reconstructTargets(
-                            FileHandler.fileToArray(fileName),
-                            FileHandler.fileToArray("backup"+fileName)
-                    );
-                    System.out.println(Arrays.toString(arrayFixed));
-                    //create new file from reconstructed data
-                    FileHandler.arrayToFile(arrayFixed,fileName);
-                    //backup the new file
-                    FileHandler.makeBackup(fileName,"backup" + fileName);
-                }
-                case "q" -> System.exit(0);
-                default -> System.out.println("Invalid input, please try again.");
+        System.out.println("\nF-15 Strike Eagle 3 Campaign Fixer");
+        System.out.println("Choose your option:");
+        System.out.println("[1] Make backup (before mission)");
+        System.out.println("[2] Fix campaign file (after mission)");
+        System.out.println("[Q] Quit");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine().toLowerCase();
+        switch (userInput) {
+            case "1" -> {
+                String fileName = FileNameBuilder.buildFileName();
+                FileHandler.makeBackup(fileName, "backup" + fileName);
             }
+            case "2" -> {
+                String fileName = FileNameBuilder.buildFileName();
+                Byte[] targetsArray = FileHandler.fileToArray(fileName);
+                if (targetsArray.length==0) {
+                    System.out.println("Restart the program and enter correct data.");
+                    break;
+                }
+                Byte[] backupTargetsArray = FileHandler.fileToArray("backup" + fileName);
+                if (backupTargetsArray.length==0) {
+                    System.out.println("There is no backup for " + fileName + " file! Restart app and make backup first.");
+                    break;
+                }
+                Byte[] arrayFixed = DataHandler.reconstructTargets(targetsArray,backupTargetsArray);
+                //create new file from reconstructed data
+                FileHandler.arrayToFile(arrayFixed, fileName);
+                //backup the new file
+                FileHandler.makeBackup(fileName, "backup" + fileName);
+            }
+            case "q" -> System.exit(0);
+            default -> System.out.println("Invalid input, please try again.");
         }
     }
 }
